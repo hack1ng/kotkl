d4311ee76273736f2dc6ae3afa3e78051dc95a8e
Ryan King <rking8888@gmail.com>
2017-10-26 18:22:58 -0700

    New styling for Register page using vuetify

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -22,9 +22,9 @@ module.exports = {
   	  	  res.status(400).send({
   	  	  	error: `The password provided failed to match the following rules:
   	  	  	<br>
-  	  	  	1. It must contain ONLY the following characters: lower case, upper case, 0-9
+  	  	  	1. It must contain ONLY the following characters: lower case, upper case, numerics.
   	  	  	<br>
-  	  	  	2. It must be at least 8 characters in length and not greater than 32 characters
+  	  	  	2. It must be at least 8 characters in length and not greater than 32 characters in length.
   	  	  `
   	  	  })
   	  	  break
